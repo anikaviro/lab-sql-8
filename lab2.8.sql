35e07184386a765538fae80f1dde6c85b03cad04
ana.vigueras <anikaviro@gmail.com>
2023-04-12 20:34:15 +0200

    SQL lab 8

---
+++ lab2.8.sql
@@ -0,0 +1,14 @@
+-- Lab 2.8 - Lab 8 SQL
+-- 1. Write a query to display for each store its store ID, city, and country.
+
+-- 2. Write a query to display how much business, in dollars, each store brought in.
+
+-- 3. Which film categories are longest?
+
+-- 4. Display the most frequently rented movies in descending order.
+
+-- 5. List the top five genres in gross revenue in descending order.
+
+-- 6. Is "Academy Dinosaur" available for rent from Store 1?
+
+-- 7. Get all pairs of actors that worked together.
